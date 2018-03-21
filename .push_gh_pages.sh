7cad6e1cf0f6ab9507a44836f0768f362eb1439f
angusmoore <18376371+angusmoore@users.noreply.github.com>
2018-03-21 17:22:11 +1100

    Fix up push_gh_pages

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -1,4 +1,4 @@
-R -e "path <- '../lubriseas'; system(paste(shQuote(file.path(R.home('bin'), 'R')), 'CMD', 'Rd2pdf', shQuote(path)))"
+#!/bin/bash
 
 rm -rf out || exit 0;
 mkdir out;
@@ -8,15 +8,18 @@ GH_REPO="@github.com/angusmoore/lubriseas.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
 for files in '*.tar.gz'; do
-  tar xfz $files
+        tar xfz $files
 done
+
+R -e "path <- '../lubriseas'; system(paste(shQuote(file.path(R.home('bin'), 'R')), 'CMD', 'Rd2pdf', shQuote(path)))"
 
 cd out
 git init
 git config user.name "travis"
 git config user.email "travis"
-cp ../tvarGIRF.pdf tvarGIRF.pdf
+
+cp ../lubriseas.pdf lubriseas.pdf
 
 git add .
-git commit -m "Rebuilt PDF package documentation and deployed to gh-pages"
+git commit -m "Auto-deploy docs to github pages"
 git push --force --quiet $FULL_REPO master:gh-pages
